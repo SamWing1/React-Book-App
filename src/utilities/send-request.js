44723fda3ff5f76106ecb870d2f43d97e67e4316
SamWing1 <geckoromanwing@hotmail.com>
2023-01-10 17:26:43 -0800

    testing sendrequest change

--- src/utilities/send-request.js
+++ src/utilities/send-request.js
@@ -11,7 +11,7 @@ export default async function sendRequest(url, method = 'GET', payload = null) {
     // Great use case for the Logical OR Assignment Operator
     options.headers ||= {...options, Authorization: `Bearer ${token}`};
   }
-  const res = await fetch(url, options);
+  const res = await fetch(options);
   // res.ok will be false if the status code is not 200-299
   // res.json() returns a promise that resolves
   // to the data that was sent back by the server
